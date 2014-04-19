58fdca6b883698bc6b876cb4d1c1a471534e87ae
s-h <apache3@foxmail.com>
2014-04-19 12:05:14 +0800

    se

--- shell/named.backup.sh
+++ shell/named.backup.sh
@@ -7,6 +7,9 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin; export PATH
 backdir=/root/backdir
 named=$backdir/named
 day=`date --date=yesterday +%Y%m%d`
+backupserverip=
+username=
+passwd=
 cp -a /var/named/chroot/etc     $named/chroot
 cp -a /var/named/chroot/var/named/*.rev $named/chroot/var
 cp -a /var/named/chroot/var/named/*.cn  $named/chroot/var
@@ -27,8 +30,8 @@ sync
 find $backdir/*.gz -mtime +2 -exec rm -fr {} \; > /dev/null 2>&1
 #################################ftp
 cd $backdir
-ftp -n 222.223.213.100 <<EOC
-user    sh      game0ver
+ftp -n $backupserverip <<EOC
+user    $username      $passwd
 binary
 cd backup/named/254
 put named254."$day".tar.gz
