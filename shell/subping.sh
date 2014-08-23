c53369f26f72a8b428262011a4e9ad8774fff17b
s-h <apache3@foxmail.com>
2014-08-23 15:08:00 +0800

    Create subping.sh

---
+++ shell/subping.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 子接口ip ping检测
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+source="221.11.109"
+test_ip="202.99.160.68"
+for sitenu in $(seq 33 62)
+do
+        ping -c 1 -w 1 -I ${source}.${sitenu} $test_ip &> /dev/null && result=0 || result=1
+        if [ "$result" == 0 ]; then
+                echo "IP ${source}.${sitenu} is OK."
+        else
+                echo "IP ${source}.${sitenu} is false."
+        fi
+done
