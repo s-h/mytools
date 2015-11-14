351c6aaa84e20f66ddca2745fac0fd3bd1e3d969
jianghao <apache3@foxmail.com>
2015-11-14 17:26:04 +0800

    Update mydig.sh

--- shell/mydig.sh
+++ shell/mydig.sh
@@ -1,76 +1,77 @@
-#!/bin/bash
-#ver 2012.9 sh
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+#!/usr/bin/env bash
+#verison 2015-11-14
+#https://github.com/s-h/mytools/shell/check.sh
 
-DNS=(175.188.160.254
-175.188.160.154
+set -f
+TXT=$1
+tag2=(
+x.x.x.x#BGP
 )
-NAME=(
-sjz-ck
-sjz-ck
+tag3=(
+x.x.x.x#BGP-NAT-178
+x.x.x.x#BGP-NAT-206
 )
-n=0
-long () {
-		for url in $i;do
-		echo "****************"
-		echo -e "\E[01;31m\033[1m$url\033[0m"
 
-			for  D in ${DNS[@]};do
-				echo "****************"
-				echo -e "\E[32;40m\033[1m$D-${NAME[$n]}>>>\033[0m"
-				dig $url @$D > /tmp/mydig.tmp
-				sed -n '/ANSWER SECTION/,/^$/p' /tmp/mydig.tmp |sed -e  's/;; ANSWER SECTION://g' -e '/^$/d'
-				grep ';; Query time:' /tmp/mydig.tmp 
-			
-				echo ""
-				let n+=1
-			done 
+function color_red (){
+echo -e "\E[01;31m\033[1m $1 \033[0m"
+}
 
-		done 
-}
-short () {
-		for url in $i;do
-			echo "****************"
-			echo -e "\E[01;31m\033[1m$url\033[0m"
-			for  D in ${DNS[@]};do
-				echo "****************"
-				echo -e "\E[32;40m\033[1m$D-${NAME[$n]}>>>\033[0m"
-				dig +short $url @$D 
-				echo ""
-				let n+=1
-			done 
+trace () {
+        dos2unix $TXT &>/dev/null
+        sed -i "/^\s*$/d" $TXT
+        for ip in $i ;do
+                traceroute -n -m 4 $ip > /tmp/tracert.txt
+                TxtTag2=`grep " 2  " /tmp/tracert.txt |sed -e 's/ 2  //g' -e 's/\s.*//g'`
+                TxtTag3=`grep " 3  " /tmp/tracert.txt |sed -e 's/ 3  //g' -e 's/\s.*//g'`
+                for i in ${tag2[@]};do
+                        tag_ip_2=`echo $i|sed 's/#.*//g'`
+                        tag_description_2=`echo $i|sed 's/.*#//g'`
+                        if [ $TxtTag2 == $tag_ip_2 ];then
+                                txt2=`color_red $tag_description_2`
+                        else
+                                txt2=$TxtTag2
+                        fi
+                done
+                for i in ${tag3[@]};do
+                        tag_ip_3=`echo $i|sed 's/#.*//g'`
+                        tag_description_3=`echo $i|sed 's/.*#//g'`
+                        if [ $TxtTag3 == $tag_ip_3 ];then
+                                txt3=`color_red $tag_description_3`
+                        else
+                                txt3=$TxtTag3
+                        fi
 
-		done 
+                done
+                echo "$ip --> $txt2 -->$txt3"
+        done
 }
-help () {
-	echo "Usage:`basename $0` [-l] url"  
-	echo "e.g. :"
-	echo " `basename $0` www.google.com"
-	echo " `basename $0` -l www.google.com"
+main () {
+while read i;do
+        if [[ "$i" =~ "##" ]];then
+                echo $i
+        elif [[ "$i" =~ "#" ]];then
+                echo >/dev/null
+        else
+                trace $i
+        fi
+done < $TXT
 }
-if [ -e "/usr/bin/dig" ];then
-	case $1 in
-		-l|-long)
-		shift
-		for i in $@;do
-		long
-		done
-		;;
-		help|-h|--help)
-		help
-		;;
-		*)
-		for i in $@;do
-		short	
-		done
-		;;
-	esac
-else
-	echo "#install dig# "
-	echo "RatHat/Centos:"
-	echo "#yum install bind-utils"
-	echo "Gentoo:"
-	echo "#emerge bind-tools"
-	echo "Debain/Ubuntu:"
-	echo "#apt-get install dnsutils"
+myhelp() {
+        echo "Usage:`basename $0` txt"
+        echo "e.g. :"
+        echo " `basename $0` bgp.txt"
+}
+if [ ! -n "$1" ];then
+        myhelp
+        exit 0
 fi
+
+
+case $1 in
+        -h|--help|help)
+        myhelp
+        ;;
+        *)
+        main
+        ;;
+esac
